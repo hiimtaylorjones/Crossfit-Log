2148b5d26881ad209d66a5da6ccbd72650b453ac
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-07-16 09:16:50 -0400

    bulking up the client server

--- client/server.js
+++ client/server.js
@@ -11,20 +11,8 @@ var db = new sqlite3.Database(':memory:');
 
 console.log("Preparing database...");
 db.serialize(function () {
-  db.run('CREATE TABLE workouts (title varchar(255), time varchar(255), description TEXT, additional_notes TEXT)')
-
-  // var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-
-  // for (var i = 0; i < 10; i++) {
-  //   stmt.run('Ipsum ' + i)
-  // }
-
-  // stmt.finalize()
-
-  // db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-  //   console.log(row.id + ': ' + row.info)
-  // })
-})
+  db.run('CREATE TABLE workouts (title TEXT, time TEXT, description TEXT additional_notes TEXT)')
+});
 console.log("Database configured");
 
 app.get('/', (req, res) => res.send('Hello World!'));
@@ -39,10 +27,56 @@ app.get('/api/hello', function(request, response) {
 app.post('/api/workouts', function(request, response) {
   console.log("Request body: ");
   console.log(request.body);
-  response.send({
-    status: 200,
-    message: "Workout created!"
-  })
+
+  var title = request.body.title;
+  var time = request.body.time;
+  var description = request.body.description;
+  var additionalNotes = request.body.additionalNotes;
+
+  var missingParams = false;
+  if (title === "") {
+    missingParams = true;
+    missingParamMessage = "Missing workout title.";
+  } else if (time === "") {
+    missingParams = true;
+    missingParamMessage = "Missing workout time.";
+  } else if (description === "") {
+    missingParams = true;
+    missingParamMessage = "Missing workout description.";
+  }
+
+  if (missingParams === true) {
+    response.send({
+      status: 500,
+      message: missingParamMessage
+    })
+  } else {
+    db.serialize(function () {
+
+      db.run("INSERT INTO workouts (title, time, description, additional_notes) VALUES(title, time, description, additional_notes)", {
+        'title': title,
+        'time': time,
+        'description': description,
+        'additional_notes': additionalNotes
+      });
+      console.log("Record created!");
+      console.log("Preparing to list database....");
+    
+      db.each('SELECT rowid AS id, title FROM workouts', function (err, row) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(row.id + ': ' + row.title);
+        }    
+      });
+      
+    });
+    db.close();
+    response.send({
+      status: 200,
+      message: "Workout created!"
+    });
+  }  
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
